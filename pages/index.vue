c3d5f45bd9d5cea87d228b124823875726f0d9c9
Agile <labhishettysaikalyan@gmail.com>
2024-02-05 19:27:29 +0530

    added info

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container mx-auto p-4">
+        <h1>Register and login to view the users!</h1>
         <!-- Display information of each registered user -->
             <div v-for="(user, index) in registeredUsers" :key="index" class="border-b border-gray-200 py-2">
             <p>Login user - {{ user.Name }}</p>
